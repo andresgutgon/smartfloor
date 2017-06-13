667b28eb3086f17bde31cfda6fba8cbb06725ab1
andresgutgon <andresgutgon@gmail.com>
2017-06-13 20:40:31 +0200

    Following get started guides

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,10 +2,17 @@ defmodule Smartfloor.HelloController do
   use Smartfloor.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    # redirect conn, to: "/hello/paco"
+    conn
+    # |> put_layout(false)
+    # |> put_status(:not_found)
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
   end
 
   def show(conn, %{"messenger" => messenger}) do
+    json conn, %{messenger: messenger}
     render conn, "show.html", messenger: messenger
   end
 end
